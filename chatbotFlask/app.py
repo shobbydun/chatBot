853f9f7b26f42bc804f193c3b104697216b56ed3
KItavi Duncan Gitau <87327873+shobbydun@users.noreply.github.com>
2024-05-30 11:02:56 +0300

    Add files via upload
    Chat bot

---
+++ chatbotFlask/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+    # https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/cc239637-9089-4f41-852f-b3bea9a29f24/DallEGeneratedImages/dalle-bd911954-8f20-444a-972c-aff47aba82250251685501404560370800.jpg&dcHint=EastUS&fileToken=0d7c21ca-58e6-4c1f-8725-823f955e52fc
